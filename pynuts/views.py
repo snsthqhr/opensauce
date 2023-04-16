2db36f550702e02f8f1d74689f51047181f45961
mondayy1 <hojoong310@gmail.com>
2023-04-17 01:00:23 +0900

    Feat: 각 페이지 연결

--- pynuts/views.py
+++ pynuts/views.py
@@ -3,25 +3,29 @@ from .models import Question, Diagnosis, GPTResponse
 from . import chatgpt
 def home(request):
     """
-    홈페이지입니다
+    홈페이지 출력
     """
     content = "영양제 추천 서비스"
     context = {'content': content}
     return render(request, 'pynuts/homepage.html', context)
 def index(request):
     """
-    question 목록(제목) 출력
+    설문지 출력
     """
-    question_list = Question.objects.order_by('content')
-    context = {'question_list': question_list}
+    context = {'question_list': ""}
     return render(request, 'pynuts/servey_page.html', context)
 def diagnosis_create(request):
     """
-    답변에 대한 gpt 진단(영양제 추천)
+    설문에 대한 gpt 진단(영양제 추천)
+    """
+
     """
     answer = Diagnosis(content=request.POST.get('content'))
     answer.save()
     gptanswer = GPTResponse(content=chatgpt.chat_gpt(answer.content))
     gptanswer.save()
     context = {'gptanswer': gptanswer}
+    """
+    content = " "
+    context = {'content': content}
     return render(request, 'pynuts/diagnosis_result.html', context)
